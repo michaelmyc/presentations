a8d78d4f8327a347624f1c8dee722dac032b2795
Michael Mao <michael.mao99@gmail.com>
2022-04-22 20:55:36 -0700

    add scss support

--- webpack.prod.js
+++ webpack.prod.js
@@ -26,8 +26,8 @@ module.exports = {
         },
       },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
